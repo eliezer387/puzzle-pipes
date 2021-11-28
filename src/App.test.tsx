ee261342ac563f1ee8dc7c65407c091254a5bfe0
Eliezer Peña <pe.eliezer@gmail.com>
2021-11-28 14:30:15 -0500

    fix(): app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { WebSocketProvider } from "./context/websocket";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders without crashing", () => {
+    render(
+      <Provider store={store}>
+        <WebSocketProvider>
+          <App />
+        </WebSocketProvider>
+      </Provider>
+    );
+  });
+
+  // it("renders the title", () => {
+  //   render(<App />);
+  //   const title = screen.getByText(/Puzzle Pipes/i);
+  //   expect(title).toBeInTheDocument();
+  // });
 });
